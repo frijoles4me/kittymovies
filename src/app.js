705b312bb45b80ae5afbd17200438658123ed2ad
alvaro cortes <frijoles4me@gmail.com>
2018-02-06 10:44:36 -0800

    init commit for kittymovies, looking good

---
+++ src/app.js
@@ -0,0 +1,10 @@
+import "jquery";
+import "./styles.scss";
+
+//window.onload = function(){
+//  console.log("Hello world this is better more")
+//}
+
+$(document).ready(function() {
+  console.log("I am working with jQuery");
+});
